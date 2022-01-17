b8d47a11473875f3bb1b4025641d3a284724263f
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2022-01-18 05:09:37 +0700

    first commit

---
+++ ProjectVirtual_0171/Assets/ScriptPergerakan.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptPergerakan : MonoBehaviour
+{
+    public Transform cameraVR;
+    public float posisiCamera = 30.0f;
+    public float kecepatan = 3.0f;
+    public bool pergerakan;
+    private CharacterController cc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc =GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if  (cameraVR.eulerAngles.x>=posisiCamera && cameraVR.eulerAngles.x<90.0f)
+        {
+             pergerakan = true;
+        }
+        else
+        {
+            pergerakan = false;
+        }
+        if (pergerakan)
+        {
+            Vector3  forward=cameraVR.InverseTransformDirection(Vector3.forward);
+            cc.SimpleMove(forward*kecepatan);
+        }
+    }
+}
